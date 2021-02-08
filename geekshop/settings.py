b41a97ee31966b67fe8e3346e3dce36de917b753
Igor <gormal98@gmail.com>
2021-02-08 14:00:04 +0300

    lesson2_5_mod

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -95,7 +95,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'geekshop',
-        'USER': 'postgres'
+        'USER': 'postgres',
+        # 'PASSWORD': 'geekbrains',
+        # 'HOST': 'localhost'
     }
 }
 
