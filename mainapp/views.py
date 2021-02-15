49d97de582ac9d59412534d28de4b6062c9869e8
Igor <gormal98@gmail.com>
2021-02-15 19:46:26 +0300

    cached_products

--- mainapp/views.py
+++ mainapp/views.py
@@ -2,7 +2,42 @@ from django.shortcuts import render
 from mainapp.models import Product, ProductCategory
 from django.core.paginator import Paginator
 
-# Create your views here.
+from django.conf import settings
+from django.core.cache import cache
+
+def get_links_menu():
+    if settings.LOW_CACHE:
+        key = 'links_menu'
+        links_menu = cache.get(key)
+        if links_menu is None:
+            links_menu = ProductCategory.objects.filter(is_active=True)
+            cache.set(key, links_menu)
+        return links_menu
+    else:
+        return ProductCategory.objects.filter(is_active=True)
+
+def get_category(pk):
+    if settings.LOW_CACHE:
+        key = f'category_{pk}'
+        category = cache.get(key)
+        if category is None:
+            category = ProductCategory.objects.get(pk=pk)
+            cache.set(key, category)
+        return category
+    else:
+        return ProductCategory.objects.get(pk=pk)
+
+def get_products_in_category_ordered_by_price(pk):
+    if settings.LOW_CACHE:
+        key = f'products_in_category_ordered_by_price_{pk}'
+        products = cache.get(key)
+        if products is None:
+            products = Product.objects.filter(category_id=pk).order_by('price')
+            cache.set(key, products)
+        return products
+    else:
+        return Product.objects.filter(category_id=pk).order_by('price')
+
 def index(request):
     context = {
         'title': 'GeekShop - главная'
@@ -11,12 +46,14 @@ def index(request):
 
 def products(request, category_id=None, page=1):
     if category_id:
-        products = Product.objects.filter(category_id=category_id).order_by('price')
+        # products = Product.objects.filter(category_id=category_id).order_by('price')
+        products = get_products_in_category_ordered_by_price(category_id)
     else:
         products = Product.objects.all()
     context = {
         'title': 'GeekShop - продукты',
-        'categories': ProductCategory.objects.all(),
+        # 'categories': ProductCategory.objects.all(),
+        'categories': get_links_menu(),
         'product_list': products
     }
     paginator = Paginator(products, 3)
